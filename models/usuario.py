2f104210488e25038e3f20b82ad507cffeec51eb
Augusto Vasconcelos <augustovnd@alu.ufc.br>
2026-05-15 18:55:44 -0300

    esboco da entidade usuario

---
+++ models/usuario.py
@@ -0,0 +1,15 @@
+from sqlmodel import SQLModel, Field, Relationship
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+	from .endereco import Endereco
+	from .pedido import Pedido
+    # from .produto import Produto
+
+class Usuario(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    nome: str
+    email: str
+    telefone: str
+    endereco: Endereco = Relationship(back_populates="usuario")
+    pedidos: list["Pedido"] = Relationship(back_populates="usuario")
+    # produtos: list["Produto"] = Relationship(back_populates="usuario")
